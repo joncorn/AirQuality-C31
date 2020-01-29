bde597ca1bbde09473ea2272293f7fe66d18df0f
jon <jdcorn@live.com>
2020-01-29 14:29:09 -0700

    add string helpers

---
+++ AirQuality-C31/Controllers/ModelControllers/JDCCityAirQualityController.h
@@ -0,0 +1,33 @@
+//
+//  JDCCityAirQualityController.h
+//  AirQuality-C31
+//
+//  Created by Jon Corn on 1/29/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JDCCityAirQuality.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+// Declaring various network fetches
+@interface JDCCityAirQualityController : NSObject
+
++ (void)fetchSupportedCountries:(void (^) (NSArray<NSString *> *_Nullable))completion;
+
++ (void)fetchSupportedCountry:(NSString *)state
+                   completion:(void (^) (NSArray<NSString *> *_Nullable))completion;
+
++ (void)fetchSupportedCitiesInState:(NSString *)state
+                            country:(NSString *)country
+                         completion:(void (^) (NSArray<NSString *> *_Nullable))completion;
+
++ (void)fetchDataForCity:(NSString *)city
+                   state:(NSString *)state
+                 country:(NSString *)country
+              completion:(void (^) (NSArray<NSString *> *_Nullable))completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AirQuality-C31/Controllers/ModelControllers/JDCCityAirQualityController.m
@@ -0,0 +1,29 @@
+//
+//  JDCCityAirQualityController.m
+//  AirQuality-C31
+//
+//  Created by Jon Corn on 1/29/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import "JDCCityAirQualityController.h"
+#import "JDCCityAirQuality.h"
+#import "JDCWeather.h"
+#import "JDCPollution.h"
+
+// String helpers
+static NSString *const baseURLString = @"https://api.airvisual.com/";
+static NSString *const versionComponent = @"v2";
+static NSString *const countryComponent = @"countries";
+static NSString *const stateComponent = @"states";
+static NSString *const cityComponent = @"cities";
+static NSString *const cityDetailsComponent = @"city";
+static NSString *const apiKey = @"5f3cd124-bdc5-4762-bb71-b81df5566cec";
+
+@implementation JDCCityAirQualityController
+
+
+
+
+
+@end
